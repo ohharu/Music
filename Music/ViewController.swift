04bb6edb8608acdae23105d0d27e8f90012c8272
ohharu <ohharu.brain@gmail.com>
2018-04-14 22:56:00 +0900

    pushし直し

--- Music/ViewController.swift
+++ Music/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer:AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func setAudioPlayer(soundName:String,type:String){
+        let soundFilePath = Bundle.main.path(forResource:soundName,ofType:type)!
+        let fileURL = URL(fileURLWithPath:soundFilePath)
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf:fileURL)
+        }
+        catch{
+            print("音楽フィルが読み込めませんでした")
+        }
+        
+    }
+    
+    @IBAction func natsuPlayButton(){
+        setAudioPlayer(soundName:"natsu",type:"mp3")
+        audioPlayer.play()
+    }
+    
+    @IBAction func amePlayButton(){
+        setAudioPlayer(soundName:"ame",type:"mp3")
+        audioPlayer.play()
+    }
+    
+    @IBAction func akiPlayButton(){
+        setAudioPlayer(soundName:"aki",type:"mp3")
+        audioPlayer.play()
+    }
 }
 
